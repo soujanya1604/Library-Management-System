c3dee8bbcf77b6f4292b46cc509c6e06a740006f
soujanya2 <gulsoujanya21@gmail.com>
2025-03-26 10:52:22 -0400

    modifying person to public class

--- Person.cs
+++ Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Library_Management_System
 {
-    class Person
+    public class Person
     {
         public string Name { get; set; }
         public string Email { get; set; }

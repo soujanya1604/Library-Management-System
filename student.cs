a4d373aca2f83334ea97c390af13f5e1ba4640ad
sreedurgakoppula <sreedurgakoppula4@gmail.com>
2025-03-26 10:55:59 -0400

    student updated

--- student.cs
+++ student.cs
@@ -11,7 +11,7 @@ namespace Library_Management_System
         public string major {get; set;}
         public string graduation_year { get; set; }
 
-        public student(string name,  string email,string id, string major, int graduation_year) : base(name, email,id)
+        public student(string name,  string email,string id, string major, string graduation_year) : base(name, email,id)
         {
             this.major = major;
             this.graduation_year = graduation_year;

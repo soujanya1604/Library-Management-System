1e9e45e1d77c0fa6af4c6e949568079bf2786406
sreedurgakoppula <sreedurgakoppula4@gmail.com>
2025-03-26 11:15:23 -0400

    updated student to Student

--- student.cs
+++ student.cs
@@ -6,21 +6,22 @@ using System.Threading.Tasks;
 
 namespace Library_Management_System
 {
-    public class student : Person
+    public class Student : Person
     {
-        public string major {get; set;}
+        public string major { get; set; }
         public string graduation_year { get; set; }
 
-        public student(string name,  string email,string id, string major, string graduation_year) : base(name, email,id)
+        public Student(string name, string email, string id, string major, string graduation_year) : base(name, email, id)
         {
             this.major = major;
             this.graduation_year = graduation_year;
         }
-        public void display() 
+        public void display()
         {
-            
-            Console.WriteLine("Major : ", major  );
+
+            Console.WriteLine("Major : ", major);
             Console.WriteLine("Graduation Year : ", graduation_year);
         }
-    
+
+    }
 }
